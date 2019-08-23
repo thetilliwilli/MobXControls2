1ae733ccc866d2fbcceaa15119394c05b430f787
TilliWilli <tilliwilli3@gmail.com>
2019-08-23 19:28:08 +0300

    minor

--- src/test1/app.tsx
+++ src/test1/app.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
-import { rawData } from "./raw-data";
 import { Pointer } from "./pointer";
-import { IntegerControl, BaseControl } from "./control";
-import { IntegerView, IntegerListView } from "./view";
-import { observable, autorun, computed } from "mobx";
+import { BaseControl } from "./control";
+import { IntegerListView } from "./view";
+import { observable } from "mobx";
 
-var ref = new Pointer(rawData.title, "maxSize");
-var control = new IntegerControl(ref);
 var data = [[1, 2, 3]];
 var observableData = observable(data);
 
@@ -22,3 +19,4 @@ var App = (
 render(App, document.getElementById("root"));
 
 setTimeout(_ => console.log(observableData[0][0]), 2000);
+observableData[0].push(1);
